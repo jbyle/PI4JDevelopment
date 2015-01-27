f32cf591a2f2c4ab144c7df7998bee4d2d60b804
Jan  Byle <jan@byle.be>
2015-01-27 21:53:40 +0100

    license information

--- src/main/java/be/byle/MPL115A2.java
+++ src/main/java/be/byle/MPL115A2.java
@@ -1,4 +1,20 @@
-package be.byle;
+/*
+Copyright [2015] [Jan Bylé]
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+
+*/
+        package be.byle;
 
 import com.pi4j.io.gpio.*;
 import com.pi4j.io.i2c.I2CBus;
@@ -174,16 +190,7 @@ public void calculateTempAndPressureReadings() throws IOException, InterruptedEx
 
     public static void main(String[] args) {
         MPL115A2 mpl115A2 = new MPL115A2();
-/*        try {
-            try {
-                mpl115A2.init(RaspiPin.GPIO_27);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-*/
+
         try {
             mpl115A2.init(RaspiPin.GPIO_27);
         } catch (IOException e) {
